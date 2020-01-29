443ce02cd090412fb97bcbbd14d8bf33d9e93675
chasegarsee <chasegarsee@gmail.com>
2020-01-29 16:08:53 -0600

    Redux complete for Signup, Login, and Logout.

--- social-family/src/App.js
+++ social-family/src/App.js
@@ -6,6 +6,8 @@ import jwtDecode from "jwt-decode";
 /* REDUX */
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
 
 /* MUI */
 
@@ -21,6 +23,7 @@ import login from "./pages/Login";
 /* COMPONENTS */
 import Navbar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
+import axios from "axios";
 
 const theme = createMuiTheme({
   palette: {
@@ -49,16 +52,16 @@ const theme = createMuiTheme({
   }
 });
 
-let authenticated;
-
 const token = localStorage.FbIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
     window.location.href = "/login";
-    authenticated = false;
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getUserData());
   }
 }
 
@@ -71,18 +74,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <AuthRoute
-                exact
-                path="/signup"
-                component={signup}
-                authenticated={authenticated}
-              />
-              <AuthRoute
-                exact
-                path="/login"
-                component={login}
-                authenticated={authenticated}
-              />
+              <AuthRoute exact path="/signup" component={signup} />
+              <AuthRoute exact path="/login" component={login} />
             </Switch>
           </div>
         </Router>
