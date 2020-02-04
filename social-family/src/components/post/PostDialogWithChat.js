2cebb0405ca2aaca214951fdc04b3a9f5e917a09
chasegarsee <chasegarsee@gmail.com>
2020-02-04 11:39:01 -0600

    added open dialog to chat button

---
+++ social-family/src/components/post/PostDialogWithChat.js
@@ -0,0 +1,187 @@
+import React, { Fragment, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../../util/MyButton";
+import dayjs from "dayjs";
+import Comments from "./Comments";
+import CommentForm from "./CommentForm";
+import { Link } from "react-router-dom";
+/* MUI */
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+/* ICONS */
+import CloseIcon from "@material-ui/icons/Close";
+import UnfoldMore from "@material-ui/icons/UnfoldMore";
+import ChatIcon from "@material-ui/icons/Chat";
+
+/* REDUX */
+import { connect } from "react-redux";
+import { getPost } from "../../redux/actions/dataActions";
+import LikeButton from "./LikeButton";
+
+const styles = {
+  invisibleSeparator: {
+    border: "none",
+    margin: 4
+  },
+  // visibleSeparator: {
+  //   width: "100%",
+  //   borderBottom: "1px solid rgba(0,0,0,0.1",
+  //   marginBottom: 20
+  // },
+  profileImage: {
+    maxWidth: 200,
+    maxHeight: 200,
+    objectFit: "cover"
+  },
+  dialogContent: {
+    padding: 20
+  },
+  closeButton: {
+    // position: "absolute",
+    // left: "80%",
+    top: "3%"
+  },
+  expandButton: {
+    // position: "absolute",
+    // left: "90%"
+  },
+  spinnerDiv: {
+    textAlign: "center",
+    margin: "50 0"
+  },
+  circularProgress: {
+    color: "rgb(245, 245, 245)"
+  }
+};
+
+function PostDialogWithChat(props) {
+  const [open, setOpen] = useState(false);
+  const [oldPath, setOldPath] = useState("");
+  const [newPath, setNewPath] = useState("");
+  useEffect(() => {
+    if (props.openDialog) {
+      handleOpen();
+    }
+  }, []);
+
+  const {
+    classes,
+    post: {
+      postId,
+      body,
+      createdAt,
+      likeCount,
+      commentCount,
+      userImage,
+      userHandle,
+      comments
+    },
+    UI: { loading }
+  } = props;
+
+  const handleOpen = () => {
+    let oldPath = window.location.pathname;
+
+    const { userHandle, postId } = props;
+    const newPath = `/users/${userHandle}/post/${postId}`;
+
+    if (oldPath === newPath) oldPath = `/users/${userHandle}`;
+
+    window.history.pushState(null, null, newPath);
+    setOpen(true);
+    setOldPath(oldPath);
+    setNewPath(newPath);
+    props.getPost(props.postId);
+  };
+
+  const handleClose = () => {
+    window.history.pushState(null, null, oldPath);
+    setOpen(false);
+  };
+
+  const dialogMarkup = loading ? (
+    <div className={classes.spinnerDiv}>
+      <LinearProgress color="secondary" />
+      <CircularProgress
+        size={200}
+        thickness={2}
+        className={classes.circularProgress}
+      />
+      <LinearProgress variant="query" color="secondary" />
+    </div>
+  ) : (
+    <Grid container spacing={5}>
+      <Grid item sm={5}>
+        <img src={userImage} alt="Profile" className={classes.profileImage} />
+      </Grid>
+      <Grid item sm={7}>
+        <Typography
+          component={Link}
+          color="primary"
+          varient="h5"
+          to={`/users/${userHandle}`}
+        >
+          @{userHandle}
+        </Typography>
+        <hr className={classes.invisibleSeparator} />
+        <Typography varient="body2" color="textSecondary">
+          {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+        </Typography>
+        <hr className={classes.invisibleSeparator} />
+        <Typography varient="body1">{body}</Typography>
+        <LikeButton postId={postId} />
+        <span>{likeCount} Likes</span>
+        <MyButton tip="Comments">
+          <ChatIcon color="secondary" />
+        </MyButton>
+        <span>{commentCount} Comments</span>
+      </Grid>
+      <CommentForm postId={postId} />
+      <Comments comments={comments} />
+    </Grid>
+  );
+  return (
+    <Fragment>
+      <ChatIcon onClick={handleOpen} color="secondary" />
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+        <MyButton
+          tip="Close"
+          onClick={handleClose}
+          tipClassName={classes.closeButton}
+        >
+          <CloseIcon />
+        </MyButton>
+        <DialogContent className={classes.dialogContent}>
+          {dialogMarkup}
+        </DialogContent>
+      </Dialog>
+    </Fragment>
+  );
+}
+
+PostDialogWithChat.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  postId: PropTypes.string.isRequired,
+  userHandle: PropTypes.string.isRequired,
+  post: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.data.post,
+  UI: state.UI
+});
+
+const mapActionsToProps = {
+  getPost
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(PostDialogWithChat));
