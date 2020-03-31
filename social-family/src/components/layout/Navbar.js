cf2adea5c101e598670288f4034c65d287f6df05
chasegarsee <chasegarsee@gmail.com>
2020-03-31 12:00:00 -0500

    beginnings of custom colors

--- social-family/src/components/layout/Navbar.js
+++ social-family/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+
 import MyButton from "../../util/MyButton";
 import { Link } from "react-router-dom";
 import Notifications from "./Notifications";
@@ -6,6 +7,7 @@ import Notifications from "./Notifications";
 /* REDUX */
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { editUserDetails } from "../../redux/actions/userActions";
 
 /* Material UI */
 import AppBar from "@material-ui/core/AppBar";
@@ -17,12 +19,24 @@ import Button from "@material-ui/core/Button";
 import HomeIcon from "@material-ui/icons/Home";
 import CreatePost from "../post/CreatePost";
 import CachedIcon from "@material-ui/icons/Cached";
+import axios from "axios";
 
 function Navbar(props) {
-  const { authenticated, refreshPage } = props;
+  const [color, setColor] = useState("");
+  const {
+    authenticated,
+    refreshPage,
+    user: {
+      credentials: { navColor }
+    }
+  } = props;
+
+  const mapUserDetailsToState = credentials => {
+    setColor(credentials.navColor ? credentials.navColor : "");
+  };
 
   return (
-    <AppBar>
+    <AppBar style={{ backgroundColor: navColor }}>
       <Toolbar className="nav-container">
         {authenticated ? (
           <Fragment>
@@ -56,11 +70,14 @@ function Navbar(props) {
 }
 
 Navbar.propTypes = {
+  editUserDetails: PropTypes.func.isRequired,
   authenticated: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
+  user: state.user,
+  credentials: state.user.credentials,
   authenticated: state.user.authenticated
 });
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps, { editUserDetails })(Navbar);
