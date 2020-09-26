75c89d243fb98261cf3b4ccb8d0f72048678f147
chasegarsee <chasegarsee@gmail.com>
2020-09-26 12:48:46 -0500

    ui changes

--- social-family/src/components/post/Post.js
+++ social-family/src/components/post/Post.js
@@ -1,113 +1,87 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import MyButton from "../../util/MyButton";
-import DeletePost from "./DeletePost";
-import PostDialog from "./PostDialog";
-import PostDialogWithChat from "./PostDialogWithChat";
-import LikeButton from "./LikeButton";
+import React from "react"
+import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
+import MyButton from "../../util/MyButton"
+import DeletePost from "./DeletePost"
+import PostDialog from "./PostDialog"
+import PostDialogWithChat from "./PostDialogWithChat"
+import LikeButton from "./LikeButton"
 
 /* REDUX */
-import { connect } from "react-redux";
+import { connect } from "react-redux"
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
 
 /* MUI */
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import withStyles from "@material-ui/core/styles/withStyles";
-import AddImageToPost from "./AddImageToPost";
+import Card from "@material-ui/core/Card"
+import CardMedia from "@material-ui/core/CardMedia"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
+import withStyles from "@material-ui/core/styles/withStyles"
+import AddImageToPost from "./AddImageToPost"
 
 const styles = {
   card: {
     position: "relative",
     display: "flex",
-    marginBottom: 20
+    marginBottom: 20,
   },
   image: {
-    minWidth: 175,
-    paddingRight: "10px",
-    borderRight: "1px solid rgba(0,0,0,0.2)"
+    width: 35,
+    height: 35,
+    borderRadius: "50%",
   },
   postImage: {
     minWidth: 200,
     minHeight: 200,
     maxHeight: 175,
-    borderRadius: 2
+    borderRadius: 2,
   },
-  content: {}
-};
+  content: {},
+}
 
 function Post(props) {
   const {
     classes,
-    post: {
-      body,
-      createdAt,
-      userImage,
-      userHandle,
-      likeCount,
-      commentCount,
-      postId,
-      imageUrl
-    },
+    post: { body, createdAt, userImage, userHandle, likeCount, commentCount, postId, imageUrl },
     user: {
       authenticated,
-      credentials: { handle, navColor }
-    }
-  } = props;
+      credentials: { handle, navColor },
+    },
+  } = props
 
-  dayjs.extend(relativeTime);
+  dayjs.extend(relativeTime)
 
   const deleteButton =
-    authenticated && userHandle === handle ? (
-      <DeletePost postId={postId} />
-    ) : null;
+    authenticated && userHandle === handle ? <DeletePost postId={postId} /> : null
   const uploadImage =
-    authenticated && userHandle === handle ? (
-      <AddImageToPost postId={postId} />
-    ) : null;
+    authenticated && userHandle === handle ? <AddImageToPost postId={postId} /> : null
 
   return (
     <Card className={classes.card}>
-      <CardMedia
-        className={classes.image}
-        image={userImage}
-        title="Profile Image"
-      />
+      {/* <CardMedia className={classes.image} image={userImage} title="Profile Image" /> */}
       <CardContent className={classes.content}>
-        <Typography
-          varient="h5"
-          component={Link}
-          to={`/users/${userHandle}`}
-          style={{ color: navColor }}
-        >
-          {userHandle}
-        </Typography>
-        <CardMedia
-          className={classes.image}
-          image={userImage}
-          component={Link}
-          to={`/users/${userHandle}`}
-          title="Profile Image"
-        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <CardMedia className={classes.image} image={userImage} title="Profile Image" />
+          <Typography
+            varient="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            style={{ color: navColor, marginLeft: "3%" }}
+          >
+            {userHandle}
+          </Typography>
+        </div>
         {uploadImage}
         {deleteButton}
-
         <Typography varient="body2" color="textSecondary">
           {dayjs(createdAt).fromNow()}
         </Typography>
         <Typography varient="body1">{body}</Typography>
         {imageUrl ? (
           <div style={{ display: "flex", justifyContent: "flex-start" }}>
-            <CardMedia
-              image={imageUrl}
-              title="Post Image"
-              className={classes.postImage}
-            />
+            <CardMedia image={imageUrl} title="Post Image" className={classes.postImage} />
           </div>
         ) : null}
 
@@ -130,18 +104,18 @@ function Post(props) {
         />
       </CardContent>
     </Card>
-  );
+  )
 }
 
 Post.propTypes = {
   user: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
-  openDialog: PropTypes.bool
-};
+  openDialog: PropTypes.bool,
+}
 
-const mapStateToProps = state => ({
-  user: state.user
-});
+const mapStateToProps = (state) => ({
+  user: state.user,
+})
 
-export default connect(mapStateToProps)(withStyles(styles)(Post));
+export default connect(mapStateToProps)(withStyles(styles)(Post))
--- social-family/src/components/profile/Profile.js
+++ social-family/src/components/profile/Profile.js
@@ -1,37 +1,38 @@
-import React, { Fragment, useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import dayjs from "dayjs";
-import EditDetails from "./EditDetails";
+import React, { Fragment, useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
+import dayjs from "dayjs"
+import EditDetails from "./EditDetails"
+import SkelUser from "../skeleton/SkelUser"
 
 /*MUI */
-import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
-import MuiLink from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import EditIcon from "@material-ui/icons/Edit";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import withStyles from "@material-ui/core/styles/withStyles"
+import Button from "@material-ui/core/Button"
+import MuiLink from "@material-ui/core/Link"
+import Typography from "@material-ui/core/Typography"
+import Paper from "@material-ui/core/Paper"
+import EditIcon from "@material-ui/icons/Edit"
+import CircularProgress from "@material-ui/core/CircularProgress"
 /*ICONS */
-import LocationOn from "@material-ui/icons/LocationOn";
-import LinkIcon from "@material-ui/icons/Link";
-import CalendarToday from "@material-ui/icons/CalendarToday";
-import KeyboardReturn from "@material-ui/icons/KeyboardReturn";
+import LocationOn from "@material-ui/icons/LocationOn"
+import LinkIcon from "@material-ui/icons/Link"
+import CalendarToday from "@material-ui/icons/CalendarToday"
+import KeyboardReturn from "@material-ui/icons/KeyboardReturn"
 
 /* REDUX */
-import { connect } from "react-redux";
-import { logoutUser, uploadImage } from "../../redux/actions/userActions";
-import MyButton from "../../util/MyButton";
+import { connect } from "react-redux"
+import { logoutUser, uploadImage } from "../../redux/actions/userActions"
+import MyButton from "../../util/MyButton"
 
-const styles = theme => ({
+const styles = (theme) => ({
   paper: {
-    padding: 20
+    padding: 20,
   },
   profileCenter: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    minHeight: 300
+    minHeight: 300,
   },
   profile: {
     "& .image-wrapper": {
@@ -40,90 +41,82 @@ const styles = theme => ({
       "& button": {
         position: "absolute",
         top: "80%",
-        left: "70%"
-      }
+        left: "70%",
+      },
     },
     "& .profile-image": {
       width: 200,
       height: 200,
       objectFit: "cover",
       maxWidth: "100%",
-      borderRadius: "50%"
+      borderRadius: "50%",
     },
     "& .profile-details": {
       textAlign: "center",
       "& span, svg": {
-        verticalAlign: "middle"
+        verticalAlign: "middle",
       },
       "& a": {
-        color: theme.palette.primary.main
-      }
+        color: theme.palette.primary.main,
+      },
     },
     "& hr": {
       border: "none",
-      margin: "0 0 10px 0"
+      margin: "0 0 10px 0",
     },
     "& svg.button": {
       "&:hover": {
-        cursor: "pointer"
-      }
-    }
+        cursor: "pointer",
+      },
+    },
   },
   buttons: {
     textAlign: "center",
     "& a": {
-      margin: "20px 10px"
-    }
-  }
-});
+      margin: "20px 10px",
+    },
+  },
+})
 
 function Profile(props) {
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0)
   const {
     classes,
     user: {
-      credentials: {
-        handle,
-        createdAt,
-        imageUrl,
-        bio,
-        website,
-        location,
-        navColor
-      },
+      credentials: { handle, createdAt, imageUrl, bio, website, location, navColor },
       loading,
-      authenticated
-    }
-  } = props;
+      authenticated,
+    },
+  } = props
 
   useEffect(() => {
     function tick() {
       // reset when reaching 100%
-      setProgress(oldProgress => (oldProgress >= 100 ? 0 : oldProgress + 1));
+      setProgress((oldProgress) => (oldProgress >= 100 ? 0 : oldProgress + 1))
     }
 
-    const timer = setInterval(tick, 20);
+    const timer = setInterval(tick, 20)
     return () => {
-      clearInterval(timer);
-    };
-  }, []);
+      clearInterval(timer)
+    }
+  }, [])
 
-  const handleImageChange = e => {
-    const image = e.target.files[0];
-    console.log(image);
-    const formData = new FormData();
-    formData.append("image", image, image.name);
-    props.uploadImage(formData);
-  };
+  const handleImageChange = (e) => {
+    const image = e.target.files[0]
+    console.log(image)
+    const formData = new FormData()
+    formData.append("image", image, image.name)
+    props.uploadImage(formData)
+  }
   const handleEditPhoto = () => {
-    const fileInput = document.getElementById("imageInput");
-    console.log("FILE IN??", fileInput);
-    fileInput.click();
-  };
+    const fileInput = document.getElementById("imageInput")
+    console.log("FILE IN??", fileInput)
+    fileInput.click()
+  }
 
   const handleLogout = () => {
-    props.logoutUser();
-  };
+    props.logoutUser()
+  }
 
   let profileMarkup = !loading ? (
     authenticated ? (
@@ -131,17 +124,8 @@ function Profile(props) {
         <div className={classes.profile}>
           <div className="image-wrapper">
             <img src={imageUrl} alt="profile" className="profile-image" />
-            <input
-              type="file"
-              id="imageInput"
-              onChange={handleImageChange}
-              hidden="hidden"
-            />
-            <MyButton
-              tip="Edit Profile Photo"
-              onClick={handleEditPhoto}
-              btnClassName="button"
-            >
+            <input type="file" id="imageInput" onChange={handleImageChange} hidden="hidden" />
+            <MyButton tip="Edit Profile Photo" onClick={handleEditPhoto} btnClassName="button">
               <EditIcon style={{ color: navColor }} />
             </MyButton>
           </div>
@@ -160,8 +144,7 @@ function Profile(props) {
             <hr />
             {location && (
               <Fragment>
-                <LocationOn style={{ color: navColor }} />{" "}
-                <span>{location}</span>
+                <LocationOn style={{ color: navColor }} /> <span>{location}</span>
                 <hr />
               </Fragment>
             )}
@@ -194,12 +177,7 @@ function Profile(props) {
         <Typography variant="body2" align="center">
           No profile found. Please login or sign up.
           <div className={classes.buttons}>
-            <Button
-              variant="contained"
-              style={{ color: navColor }}
-              component={Link}
-              to="/login"
-            >
+            <Button variant="contained" style={{ color: navColor }} component={Link} to="/login">
               Login
             </Button>
             <Button
@@ -217,31 +195,23 @@ function Profile(props) {
     )
   ) : (
     <div className={classes.profileCenter}>
-      <CircularProgress
-        className={classes.CircularProgress}
-        variant="determinate"
-        value={progress}
-        size={100}
-      />
+      <SkelUser />
     </div>
-  );
-  return profileMarkup;
+  )
+  return profileMarkup
 }
 
-const mapStateToProps = state => ({
-  user: state.user
-});
+const mapStateToProps = (state) => ({
+  user: state.user,
+})
 
-const mapActionsToProps = { logoutUser, uploadImage };
+const mapActionsToProps = { logoutUser, uploadImage }
 
 Profile.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
-};
+  classes: PropTypes.object.isRequired,
+}
 
-export default connect(
-  mapStateToProps,
-  mapActionsToProps
-)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
